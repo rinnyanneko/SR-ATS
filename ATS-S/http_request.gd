41e65028e6e4b6b2706999638a1e1073c1561494
Your Name <you@example.com>
2024-09-18 15:58:57 +0800

    20240918

--- ATS-S/http_request.gd
+++ ATS-S/http_request.gd
@@ -2,6 +2,7 @@ extends HTTPRequest
 var cfg = ConfigFile.new()
 var err = cfg.load("res://config.cfg")
 var tmp = ConfigFile.new()
+var errors = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,16 +21,22 @@ func _on_request_completed(result: int, response_code: int, headers: PackedStrin
 	var json:Dictionary = JSON.parse_string(body.get_string_from_utf8())
 	var dataArray:Array = json["data"]
 	var data = readArray(dataArray)
-	tmp.set_value("TrainData", "ControlledBySteamID", data["ControlledBySteamID"])
-	tmp.set_value("TrainData", "InBorderStationArea", data["InBorderStationArea"])
-	tmp.set_value("TrainData", "Latititute", data["Latititute"])
-	tmp.set_value("TrainData", "Longitute", data["Longitute"])
-	tmp.set_value("TrainData", "Velocity", data["Velocity"])
-	tmp.set_value("TrainData", "SignalInFront", data["SignalInFront"])
-	tmp.set_value("TrainData", "DistanceToSignalInFront", data["DistanceToSignalInFront"])
-	tmp.set_value("TrainData", "SignalInFrontSpeed", data["SignalInFrontSpeed"])
-	tmp.set_value("TrainData", "VDDelayedTimetableIndex", data["VDDelayedTimetableIndex"])
-	tmp.save("res://ATS-S/data.tmp")
+	if data != null and data["ControlledBySteamID"] != null and data["InBorderStationArea"] != null and data["Latititute"] != null and data["Longitute"] != null and data["Velocity"] != null and data["SignalInFront"] != null and data["DistanceToSignalInFront"] != null and data["SignalInFrontSpeed"] != null and data["VDDelayedTimetableIndex"] != null:
+		tmp.set_value("TrainData", "ControlledBySteamID", data["ControlledBySteamID"])
+		tmp.set_value("TrainData", "InBorderStationArea", data["InBorderStationArea"])
+		tmp.set_value("TrainData", "Latititute", data["Latititute"])
+		tmp.set_value("TrainData", "Longitute", data["Longitute"])
+		tmp.set_value("TrainData", "Velocity", data["Velocity"])
+		tmp.set_value("TrainData", "SignalInFront", data["SignalInFront"])
+		tmp.set_value("TrainData", "DistanceToSignalInFront", data["DistanceToSignalInFront"])
+		tmp.set_value("TrainData", "SignalInFrontSpeed", data["SignalInFrontSpeed"])
+		tmp.set_value("TrainData", "VDDelayedTimetableIndex", data["VDDelayedTimetableIndex"])
+		tmp.save("res://ATS-S/data.tmp")
+	elif errors < 10:
+		errors += 1
+	else:
+		$"../AcceptDialog".visible = true
+		get_tree().change_scene_to_file("res://main.tscn")
 
 func readArray(array:Array):
 	for data in array:
--- save.gd
+++ save.gd
@@ -1,4 +1,4 @@
-extends TextureButton
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,4 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://ATS-S/ats_s_scene.tscn")
+	if $"../Server".text != null:
+		$"../ATS-S".disabled = false
+		#$"../ATS-P".disabled = false
+		#$"../ATS-Ps".disabled = false
