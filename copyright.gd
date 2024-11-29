0ac377a5d97cb43d8428e0bcc44d5943c0cba073
凛喵 <rinnyanneko@outlook.com>
2024-11-29 18:42:19 +0800

    change file name

--- copyright.gd
+++ copyright.gd
@@ -15,4 +15,4 @@ func _on_pressed() -> void:
 	cfg.load("res://config.cfg")
 	cfg.set_value("License", "DoNotShow", "false")
 	cfg.save("res://config.cfg")
-	get_tree().change_scene_to_file("res://license.tscn")
+	get_tree().change_scene_to_file("res://EULA.tscn")
