2f0cd4a1722d669a6d76614c5c632fa244a8d9fc
凛喵 <rinnyanneko@outlook.com>
2024-12-06 15:35:33 +0800

    upadte news(test)

--- news_request.gd
+++ news_request.gd
@@ -1,5 +1,6 @@
 extends HTTPRequest
 var cfg = ConfigFile.new()
+var json:Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,9 +13,24 @@ func _process(delta: float) -> void:
 
 func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	cfg.load("res://config.cfg")
-	var json:Dictionary = JSON.parse_string(body.get_string_from_utf8())
-	var title = json["title"]
-	$"../news".title = self.title
-	$"../news".text = json["text"]
-	if json["type"] != "" and json["number"] != cfg.get_value():
-		
+	json = JSON.parse_string(body.get_string_from_utf8())
+	$"../news".title = json["title"]
+	$"../news".dialog_text = json["text"]
+	$"../news".cancel_button_text = tr("CLOSE")
+	$"../news".ok_button_text = tr("DO_NOT_SHOW")
+	if json["image"] != "false":
+		$"../news".dialog_text += "[img]" + json["image"] + "[/img]"
+	print(json)
+	if json["type"] != "" and json["number"] != cfg.get_value("News", "NeverShow"):
+		$"../news".visible = true
+
+
+func _on_news_confirmed() -> void:
+	cfg.set_value("News", "NeverShow", String(json["number"]))
+	cfg.save("res://config.cfg")
+	print("Never show this news")
+	$"../news".visible = false
+
+func _on_news_canceled() -> void:
+	$"../news".visible = false
+	print("News closed")
