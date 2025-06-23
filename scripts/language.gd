9b74d0feac01ae3b5b0f6572aa72fb7fa8ba239d
凛喵 <rinnyanneko@outlook.com>
2025-06-23 21:59:37 +0800

    change installer and license

--- scripts/language.gd
+++ scripts/language.gd
@@ -1,6 +1,9 @@
 # SR-ATS
 # https://github.com/rinnyanneko/SR-ATS
-# Copyright © 2025 rinnyanneko. All rights reserved.
+# Copyright © 2025 rinnyanneko. Some Rights Reserved.
+# Licensed under the Apache License, Version 2.0
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
 
 extends TextureButton
 var cfg = ConfigFile.new()
